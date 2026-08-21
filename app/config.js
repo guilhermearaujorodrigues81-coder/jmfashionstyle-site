06bcdd25744f1d05ae06ac259f5b5f6dabe9d773
guilhermearaujorodrigues81-coder <guilhermearaujorodrigues81@gmail.com>
2026-08-21 10:32:56 -0300

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,5 +1,4 @@
-
 window.STUDIO_CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_SUA_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "COLE_AQUI_SUA_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://vkuywiqckcysytjckamm.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_1Y32uQxh_5i0If9LM2gaVA_bsviNfAv"
 };
